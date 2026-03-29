60e67bc28fa69526b99a0d8c638f8c7f0d45b1ba
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-03-29 20:01:54 -0300

    Criando TransacaoService

--- src/main/java/com/ControleFinanceiro/Desafio/repository/CategoriaRepository.java
+++ src/main/java/com/ControleFinanceiro/Desafio/repository/CategoriaRepository.java
@@ -3,6 +3,8 @@ package com.ControleFinanceiro.Desafio.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ControleFinanceiro.Desafio.model.Categoria;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
---
+++ src/main/java/com/ControleFinanceiro/Desafio/service/TransacaoService.java
@@ -0,0 +1,45 @@
+package com.ControleFinanceiro.Desafio.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ControleFinanceiro.Desafio.model.Transacao;
+import com.ControleFinanceiro.Desafio.repository.TransacaoRepository;
+
+@Service
+public class TransacaoService {
+
+    private final TransacaoRepository transacaoRepository;
+
+    public TransacaoService(TransacaoRepository transacaoRepository) {
+        this.transacaoRepository = transacaoRepository;
+    }
+
+    public Transacao createTransacao(Transacao transacao) {
+        return transacaoRepository.save(transacao);
+    }
+
+    public List<Transacao> getAllTransacoes() {
+        return transacaoRepository.findAll();
+    }
+
+    public Transacao getTransacaoById(Long id) {
+        return transacaoRepository.findById(id).orElse(null);
+    }
+
+    public Transacao updateTransacao(Long id, Transacao transacao) {
+        Transacao existeTransacao = transacaoRepository.findById(id).orElse(null);
+        if (existeTransacao != null) {
+            existeTransacao.setValor(transacao.getValor());
+            existeTransacao.setData(transacao.getData());
+            existeTransacao.setCategoria(transacao.getCategoria());
+            return transacaoRepository.save(existeTransacao);
+        }
+        return null;
+    }
+
+    public void deleteTransacao(Long id) {
+        transacaoRepository.deleteById(id);
+    }
+}
