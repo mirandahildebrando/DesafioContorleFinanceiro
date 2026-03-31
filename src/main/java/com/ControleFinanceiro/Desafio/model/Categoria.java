604905362ef8c921aad7b61d9907f58d2073ce60
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-03-31 16:18:16 -0300

    Controlando que o campo categoria nao pode repetir o nome

--- src/main/java/com/ControleFinanceiro/Desafio/model/Categoria.java
+++ src/main/java/com/ControleFinanceiro/Desafio/model/Categoria.java
@@ -1,10 +1,10 @@
 package com.ControleFinanceiro.Desafio.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +20,7 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String nome;
     private String tipo; // "receita" ou "despesa"
 
